fc433528cf74cd51b71749d28be8c7c5bee0f8b9
ASUSA <ay.shim16@gmail.com>
2021-02-09 11:53:20 +0000

    04 create products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_120045) do
+ActiveRecord::Schema.define(version: 2021_02_09_112239) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
