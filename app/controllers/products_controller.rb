fd8ef36ccba73e7e099b469f163ef2569ef055cd
ASUSA <ay.shim16@gmail.com>
2021-02-11 01:01:10 +0000

    09 create_reviews

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
   def index
     @products = Product.all
   end
@@ -10,34 +11,36 @@ class ProductsController < ApplicationController
   end
 
   def new
-   @product = Product.new
-   @categories = Category.all
+    @product = Product.new
+    @categories = Category.all
   end
 
   def create
-   @product = Product.new(product_params)
-   @product.save
-   redirect_to product_url @product
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to product_url @product
   end
 
   def edit
   end
 
   def update
-   @product.update(product_params)
-   redirect_to product_url @product
+    @product.update(product_params)
+    redirect_to product_url @product
   end
 
   def destroy
-    product.destroy
+    @product.destroy
     redirect_to products_url
   end
-  
+
   private
-　　  def set_product
+
+    def set_product
       @product = Product.find(params[:id])
     end
-   def product_params
-   params.require(:product).permit(:name, :description, :price, :category_id)
-   end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :category_id)
+    end
 end
